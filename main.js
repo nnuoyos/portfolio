37b6a16f326dfe9d4631e2d6c37957fdd4af15e1
nnuoyos <youngadult3104@gmail.com>
2022-09-24 23:54:42 +0900

    모바일,태블릿 사이즈 마크업

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
-/* 모바일 사이즈 - 페이지 하단 toggle button */
-//변수 지정
-const toggleBtn = document.querySelector('.side_toggle_button');
-const menu_list = document.querySelector('.toggle_menu_list');
-//toggle button click 
-toggleBtn.addEventListener('click', (event)=>{
-    menu_list.classList.toggle('active');
-    event.currentTarget.classList.toggle('active-1');
-});
+/* mobile - side toggle button */
+$('.side_toggle_button').click(function(event){
+    event.stopPropagation(); //부모태그로의 이벤트 stop
+    $('.toggle_menu').slideToggle();
+});  
 
 /* toggle button click시 해당 영역으로 스크롤 이동 */
 $(function (){
-    $('toggle_menu_list > li ').click(function(){
-        $('html, body').animate({scrollTop: $(this.hash).offset.top}, 300);
+    $('toggle_menu_list > li ').click(function(event){
+        event.preventDefault();
+        x = $(this).attr("href");
+        $('html, body').stop().animate({scrollTop: $(x).offset().top-140}, 1000);
     });
 });
